35df1f47cca29de560d155b7c0a0cf8f979f6bc2
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-10-11 22:15:13 -0300

    desafio finalizado

--- contador/src/components/main.jsx
+++ contador/src/components/main.jsx
@@ -3,23 +3,54 @@ import { useState } from "react";
 export default function Main(){
 
   const [numero, setNumero] = useState(0);
+  const [numero2, setNumero2] = useState(0);
 
   const aumentar = () => {
     if(numero >= 0 && numero <10) setNumero(numero + 1);
   }
 
+  const aumentar2 = () => {
+    if(numero2 >= 0 && numero2 <10) setNumero2(numero2 + 1);
+  }
+
   const diminuir = () => {
     if(numero>0) setNumero(numero - 1);
+  }
+
+  const diminuir2 = () => {
+    if(numero2>0) setNumero2(numero2 - 1);
+  }
+
+  const zerar = () => {
+    setNumero(0);
+    setNumero2(0);
+  }
+
+  const trocar = () => {
+    setNumero(numero2);
+    setNumero2(numero);
   }
     return(
         <>
          <main>
-                <p>{numero}</p>
              <section>
-                    
-                    <button onClick={diminuir}>menos</button>
-                    <button onClick={aumentar}>mais</button>
-                </section>
+                <input type="text" name="time1" id="time1" />
+
+                <input type="button" name="placar1" id="placar1" value={numero} />
+                <input type="button" name="placar2" id="placar2" value={numero2} />
+
+                <input type="text" name="time2" id="time2" />
+                  <div className="break" />
+                <button onClick={aumentar}>Aumentar Esquerda</button>
+                <button onClick={aumentar2}>Aumentar Direita</button>
+                <div className="break" />
+                <button onClick={diminuir}>Diminuir Esquerda</button>
+                <button onClick={diminuir2}>Diminuir Direita</button>
+                <div className="break" />
+                <button onClick={trocar}>Trocar</button>
+                <div className="break" />
+                <button onClick={zerar}>Zerar</button>
+              </section>
          </main>
         </>
     )
